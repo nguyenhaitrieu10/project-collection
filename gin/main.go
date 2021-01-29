c8b2f52d77b4b7d549f419b477d5769e8f335dcf
Tony <haitrieu.nguyen@foodycorp.vn>
2021-01-29 09:51:30 +0700

    certs

--- gin/main.go
+++ gin/main.go
@@ -4,10 +4,16 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "success",
 		})
 	})
-	r.Run("0.0.0.0:8080")
+
+	// r.Run("0.0.0.0:8080")
+	r.RunTLS(
+		"0.0.0.0:8080",
+		"./envoy/certs/gin.example.com.crt",
+		"./envoy/certs/gin.example.com.key",
+	)
 }
